f2ab1a91117fdbf51d071d5ce6df783536c6b4df
paphonwat <paphonwat.w@gmail.com>
2024-05-16 14:09:36 +0700

    feat:add ex1-6

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,8 @@
 // Exercise #3: Find a Minimum Score
 let scores = [100, 20, 3, 1000];
-let minScore;
+let minScore = scores[0];
 // Start coding here
-
+for (let i = 1; i < scores.length ; i++) {
+    minScore = Math.min(minScore, scores[i])
+}
 console.log(minScore);
--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,7 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
+for (let i = 0; i < restaurants.length; i++) {
+    console.log(`Restaurant Name: ${restaurants[i]}`)
+}
 
-console.log(newRestaurants);
