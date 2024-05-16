e69c2aafef9d6fe0c841b3c4448bbb46a96dd31b
Namfon <varisa.vb@hotmail.com>
2024-05-16 15:07:13 +0700

    feat(ex-6): Add another way to Reverse the String

--- ex-6.js
+++ ex-6.js
@@ -3,8 +3,14 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
-console.log(companyName.length);
+// console.log(companyName.length);
 for (let i = companyName.length - 1; i >= 0; i--) {
   reversedCompanyName += companyName[i];
 }
 console.log(reversedCompanyName);
+
+// Another way to reverse string
+// for (let i = 0; i < companyName.length; i++) {
+//   reversedCompanyName=companyName[i]+reversedCompanyName;
+// }
+// console.log(reversedCompanyName);
